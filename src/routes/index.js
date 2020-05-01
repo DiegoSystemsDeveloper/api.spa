7d2cc6ec65e4e145dcf28ba9d918fb2250520762
Diego perez <hhola3628@gmail.com>
2020-05-01 17:49:48 -0500

    se agrega funcion para crear citas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getManicurists, getCustomers, getProducts, getServices, getAppoinments } = require('../controllers/index.controllers');
+const { getManicurists, getCustomers, getProducts, getServices, getAppoinments, insertAppoinment } = require('../controllers/index.controllers');
 
 router.get('/manicurists', getManicurists);
 router.get('/customers', getCustomers);
 router.get('/products', getProducts);
 router.get('/services', getServices);
 router.get('/appoinments', getAppoinments);
-router.post('/appoinments');
+router.post('/appoinments', insertAppoinment);
 
 module.exports = router;
